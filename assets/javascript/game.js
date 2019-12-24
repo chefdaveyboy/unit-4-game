0d5b3f243f3ac73218ebfff5c65cec42c30c1565
David Koeller <david.koeller@gmail.com>
2019-12-23 18:34:23 -0600

    psuedo-coded game.js & added a reset.css

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Randomly choose a target number between 19-120.
+
+//Create and object for each crystal.
+//Assign a random value to each crystal between 1-12.
+    //Hide the random value until the crystal is selected.
+    //Make sure that each crystal is a different value?
+
+//on click event for when a crystal is clicked on
+    //check to see if the players "score" is equal to the target score.
+    //if score = target, the player wins!
+    //if score < target, the game continues.
+    //if score > target, the game is over.
+
+//create a reset fuction to reset the game after a win or a loss.
+    //New Target Number
+    //New Crystal Values
+    //Score counter = 0
+
+//Display games won and lost.
