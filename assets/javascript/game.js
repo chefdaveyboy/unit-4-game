e0ebe9faa44eb531b759728af3b35c10cac02223
David Koeller <david.koeller@gmail.com>
2019-12-31 18:02:18 -0600

    basic css done & started on game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,19 @@
 //Randomly choose a target number between 19-120.
+var targetNumber = Math.floor(Math.random() * 101) + 19;
+//Assign a random value to each crystal between 1-12.
+var crystalNumber = Math.floor(Math.random() * 12) + 1;
+
+//assign the random targetNumber to the random-crystal div.
+$("#random-crystal").text("Target Collection: " + targetNumber);
+
+//Create base score values
+var currentScore = 0;
+var winCount = 0;
+var lossCount = 0;
+
 
 //Create and object for each crystal.
-//Assign a random value to each crystal between 1-12.
+
     //Hide the random value until the crystal is selected.
     //Make sure that each crystal is a different value?
 
@@ -12,6 +24,11 @@
     //if score > target, the game is over.
 
 //create a reset fuction to reset the game after a win or a loss.
+function reset() {
+    targetNumber = Math.floor(Math.random() * 101) + 19;
+    crystalNumber = Math.floor(Math.random() * 12) + 1;
+    currentScore = 0;
+}
     //New Target Number
     //New Crystal Values
     //Score counter = 0
