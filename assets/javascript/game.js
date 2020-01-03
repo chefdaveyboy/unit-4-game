fd8e212c89d49f77ae064f7a267b26b79708ed5a
David Koeller <david.koeller@gmail.com>
2020-01-02 22:01:41 -0600

    functional game, needs polishing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 //Randomly choose a target number between 19-120.
 var targetNumber = Math.floor(Math.random() * 101) +19;
-//Assign a random value to each crystal between 1-12.
+
 
 $("#random-crystal").text("Target Collection: " + targetNumber);
 //assign the random targetNumber to the random-crystal div.
 
+// var crystalNumber = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Create base score values
 var currentScore = 0;
@@ -13,12 +14,18 @@ var lossCount = 0;
 //reset function
 function reset() {
     targetNumber = Math.floor(Math.random() * 101) +19;
-    crystalNumber = Math.floor(Math.random() * 12) + 1;
+    $(".crystal-button").each(function crystalValue() {
+        //creates the random number
+        crystalNumber = Math.floor(Math.random() * 12) + 1;
+        //applies the number to each .crytstal-button
+        $(this).attr("crystal-value", crystalNumber);
+    })
     currentScore = 0;
+ 
     $("#random-crystal").text("Target Collection: " + targetNumber);
     
 }
-    
+
 
 //The Game
 $(document).ready(function () {
@@ -28,10 +35,11 @@ $(document).ready(function () {
         crystalNumber = Math.floor(Math.random() * 12) + 1;
         //applies the number to each .crytstal-button
         $(this).attr("crystal-value", crystalNumber);
-
-        console.log(crystalNumber);
+        
 
     })
+
+    
 
 
     //When a crystal is clicked the value is added to the user's score.
@@ -40,15 +48,19 @@ $(document).ready(function () {
         $("#total-crystal").text("Total Collected: " + currentScore);
     //if the total collected equals the target collection, the player wins!
     if (currentScore === targetNumber) {
-        winCount++
+        winCount++;
         //adds 1 to the win text
         $("#wins").text("Wins: " + winCount);
         //game resets
         reset();
+        targetNumber = Math.floor(Math.random() * 101) +19;
+    crystalNumber = Math.floor(Math.random() * 12) + 1;
+    currentScore = 0;
+    $("#random-crystal").text("Target Collection: " + targetNumber);
     }
     //if the total collected exceeds the target collection, the player loses!
     else if (currentScore > targetNumber) {
-        lossCount++
+        lossCount++;
         //add 1 to the loss text
         $("#losses").text("Losses: " + lossCount);
         //game resets
@@ -60,12 +72,3 @@ $(document).ready(function () {
     
 
 })
-
-
-//create a reset fuction to reset the game after a win or a loss.
-
-    //New Target Number
-    //New Crystal Values
-    //Score counter = 0
-
-//Display games won and lost.
